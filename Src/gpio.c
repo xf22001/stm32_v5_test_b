8bda8d17f253d7aab130b53a192ed17359cd510f
xiaofei <alibaba22001@163.com>
2022-05-16 13:30:52 +0800

    update

--- Src/gpio.c
+++ Src/gpio.c
@@ -129,7 +129,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : PBPin PBPin */
   GPIO_InitStruct.Pin = can_int0_Pin|can_int1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
